2aa939f2a1b7633ca890d0f17948b109cb4c2340
Mdraihanahmad <mdraihanahmad47@gmail.com>
2025-08-11 20:56:46 +0530

    fix: Update server log message for clarity

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,5 @@ const connectToDB = require("./src/db/db");
 //database
 connectToDB()
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server running on port 3000 ....");
 });
